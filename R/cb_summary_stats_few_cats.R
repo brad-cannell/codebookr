cdb0241873206906a7bb67f607088a957ebfaf9d
Brad Cannell <brad.cannell@gmail.com>
2022-05-04 10:31:37 -0500

    Remove bfuncs from cb_summary_stats_few_cats
    Discussed in #3
    
    The use of bfuncs to calculate categorical statistics is deprecated. The freq_table() function is now part of the freqtables package. However, we don't really need to use freqtables to calculate the n, cumulative n, and percentage. I'm just using standard dplyr functions to do these calculations now.

--- R/cb_summary_stats_few_cats.R
+++ R/cb_summary_stats_few_cats.R
@@ -13,26 +13,37 @@ cb_summary_stats_few_cats <- function(df, .x, digits = 2) {
   var = n = cum_freq = percent = NULL
 
   # ===========================================================================
-  # Variable management
-  # ===========================================================================
-  x <- rlang::sym(.x)
-
-  # ===========================================================================
   # Calculate measures of interest
   # ===========================================================================
   summary <- df %>%
-    dplyr::group_by(!!x) %>%
-    bfuncs::freq_table(digits = digits) %>%
+    dplyr::count({{ .x }}) %>%
+    # Rename the first column from the name of the variable being analyzed to
+    # "cat"
+    dplyr::rename(cat = 1) %>%
+    # Change the category label for missing values from NA to "Missing"
     dplyr::mutate(cat = tidyr::replace_na(cat, "Missing")) %>%
-    # Change overall total to cumulative total
-    dplyr::mutate(cum_freq = cumsum(n)) %>%
-    dplyr::select(cat, n, percent, cum_freq) %>%
-
+    # Calculate the cumulative total and percentage
+    dplyr::mutate(
+      cum_freq = cumsum(n),
+      prop     = n / max(cum_freq),
+      percent  = prop * 100
+    ) %>%
+    # Keep columns of interest
+    dplyr::select(cat, n, cum_freq, percent) %>%
     # Format numeric results
-    dplyr::mutate_all(format, nsmall = digits, big.mark = ",")
+    dplyr::mutate(
+      dplyr::across(
+        .cols = c(n, cum_freq, percent),
+        .fns  = ~ format(.x, nsmall = digits, big.mark = ",")
+      )
+    )
 
   # ===========================================================================
   # Return tibble of results
   # ===========================================================================
   summary
 }
+
+# For testing
+# data(study)
+# cb_summary_stats_few_cats(study, sex, digits = 2)
