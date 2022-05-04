3d3befcb7b5bc2d26d51b2db0156ac850def9ef4
Brad Cannell <brad.cannell@gmail.com>
2022-05-04 10:58:52 -0500

    Create unit test file structure
    Discussed in #3

---
+++ tests/testthat/test-cb_summary_stats_few_cats.R
@@ -0,0 +1,29 @@
+df <- cb_summary_stats_few_cats(study, sex, digits = 2)
+
+testthat::test_that("Dimensions of the object returned by cb_summary_stats_few_cats are as expected", {
+  rows    <- nrow(df)
+  columns <- ncol(df)
+
+  testthat::expect_equal(rows, 3L)
+  testthat::expect_equal(columns, 4L)
+})
+
+testthat::test_that("The expected column names are returned by cb_summary_stats_few_cats", {
+  testthat::expect_equal(names(df), c("cat", "n", "cum_freq", "percent" ))
+})
+
+testthat::test_that("The correct category levels are returned by cb_summary_stats_few_cats", {
+  levels <- df$cat
+  testthat::expect_equal(levels, c("Female", "Male", "Missing"))
+})
+
+testthat::test_that("The correct default statistics are returned by freq_table", {
+  testthat::expect_equal(df$n,    c("11", " 8", " 1"))
+  testthat::expect_equal(df$cum_freq, c("11", "19", "20"))
+  testthat::expect_equal(df$percent, c("55.00", "40.00", " 5.00"))
+})
+
+# =============================================================================
+# Clean up
+# =============================================================================
+rm(df)
