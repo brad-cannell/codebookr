cca243bb95687078109bf13b458fcd185359bc83
Brad Cannell <brad.cannell@gmail.com>
2022-05-06 11:35:22 -0500

    Fix typo in test-cb_summary_stats_few_cats
    Discussed in #3

--- tests/testthat/test-cb_summary_stats_few_cats.R
+++ tests/testthat/test-cb_summary_stats_few_cats.R
@@ -17,7 +17,7 @@ testthat::test_that("The correct category levels are returned by cb_summary_stat
   testthat::expect_equal(levels, c("Female", "Male", "Missing"))
 })
 
-testthat::test_that("The correct default statistics are returned by freq_table", {
+testthat::test_that("The correct default statistics are returned by cb_summary_stats_few_cats", {
   testthat::expect_equal(df$n,    c("11", " 8", " 1"))
   testthat::expect_equal(df$cum_freq, c("11", "19", "20"))
   testthat::expect_equal(df$percent, c("55.00", "40.00", " 5.00"))
