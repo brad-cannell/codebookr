2ea48863ae0696c2da54cc21834f59a2a9ee570f
Brad Cannell <brad.cannell@gmail.com>
2022-05-08 15:25:07 -0500

    Fix error: the condition has length > 1
    Part of #3
    
    the condition has length > 1 and only the first element will be used

--- R/cb_add_summary_stats.R
+++ R/cb_add_summary_stats.R
@@ -48,28 +48,38 @@ cb_add_summary_stats <- function(df,
     col_type <- "guess"
   }
 
-  # Try to guess col_type where needed
-  # If the user doesn't like these guesses, they can always go back and
-  # explicitly set the col_type attribute
+  # Try to guess col_type if col_type wasn't set by the user.
+  # The guess is made from a combination of the columns class and the number
+  # of unique non-missing values it has -- compared to the value of many_cats
+  # (logical, character, factor) or the value of num_to_cat (numeric).
+  # The user can avoid the guessing algorithm by explicitly set the col_type
+  # attribute
   if (col_type == "guess") {
 
     # Get class
     x_class <- class(df[[.x]])
     x_class
 
-    # Try to guess from class
     # Logical, character, and factor are categorical
-    if (x_class %in% c("logical", "character", "factor")){
-      # Figure out if they have many categories or few categories
+    if ("logical" %in% x_class) {
+      # Logical can only take three values (T/F/NA), so it should always be
+      # few_cats.
+      # Theoretically, someone could set many_cats to 1 and this
+      # would return unexpected results (few_cats instead of many_cats),
+      # but that seems unlikely in practice.
+      col_type <- "few_cats"
+    } else if ("character" %in% x_class || "factor" %in% x_class) {
+      # For character and factor, figure out if they have many categories
+      # or few categories (compared to the value of many_cats)
       if (n_unique_vals >=  many_cats) {
         col_type <- "many_cats"
       } else {
         col_type <- "few_cats"
       }
-      # Integer and numeric may be numeric or categorical
+    } else if ("integer" %in% x_class || "numeric" %in% x_class) {
+      # Integer and numeric may be numeric or categorical (e.g. likert)
       # Use num_to_cat as a cut-off value for determining if a numeric variable
       # is really categorical
-    } else if (x_class %in% c("integer", "numeric")) {
       # If n_unique_vals is <= num_to_cat then we guess categorical
       if (n_unique_vals <= num_to_cat) {
         # If guessed categorical, then determine if many cats or few cats
@@ -78,11 +88,16 @@ cb_add_summary_stats <- function(df,
         } else {
           col_type <- "few_cats"
         }
-        # If n_unique_vals is > num_to_cat then we guess numeric
+      # If n_unique_vals is > num_to_cat then we guess numeric
       } else {
         col_type <- "numeric"
       }
-    } else if (x_class %in% c("Date", "POSIXct", "POSIXt")) {
+    } else if ("Date" %in% x_class || "POSIXct" %in% x_class ||
+               "POSIXt" %in% x_class || "hms" %in% x_class ||
+               "difftime" %in% x_class) {
+      # If it is one of these common date/time classes, then set col_type to
+      # time. If it is some other date/time class, the user can always manually
+      # set the col_type attribute equal to "time".
       col_type <- "time"
     } else {
       stop("Column ", .x, " is of unknown type. Please set the col_type attribute")
