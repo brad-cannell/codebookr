7667b44ebe158ee24698e4f4fb36900b5723fc5c
Brad Cannell <brad.cannell@gmail.com>
2022-06-21 14:34:40 -0500

    Add documentation for val_labels to cb_add_col_attributes
    Discussed in #3

--- R/cb_get_col_attributes.R
+++ R/cb_get_col_attributes.R
@@ -3,17 +3,88 @@
 #' @description Used in codebook() to create the top half of the column attributes
 #' table.
 #'
+#' @details Typically, though not necessarily, the first step in creating your
+#' codebook will be to add column attributes to your data. The
+#' `cb_add_col_attributes()` function is a convenience function that allows you
+#' to add arbitrary attributes to columns (e.g., description, source, column type).
+#' These attributes can later be accessed to fill in the column attributes table
+#' of the codebook document. Column attributes _can_ serve a similar function
+#' to variable labels in SAS or Stata; however, you can assign many different
+#' attributes to a column and they can contain any kind of information you want.
+#'
+#' Although the `cb_add_col_attributes()` function will allow you to add any
+#' attributes you want, there are currently **only four** special attributes
+#' that the `codebook()` function (via `cb_get_col_attributes()`) will recognize
+#' and add to the column attributes table of the codebook document. They are:
+#'
+#' * **description**: Although you may add any text you desire to the `description`
+#' attribute, it is intended to be used to describe the question/process that
+#' generated the data contained in the column. Many statistical software packages
+#' refer to this as a variable label.
+#'
+#' * **source**: Although you may add any text you desire to the `source`
+#' attribute, it is intended to be used to describe where the data contained in
+#' the column originally came from. For example, if the current data frame was
+#' created by merging multiple data sets together, you may want to use the
+#' source attribute to identify the data set it originates from. As another
+#' example, if the current data frame contains longitudinal data, you may want
+#' to use the source attribute to identify the wave(s) in which data for this
+#' column was collected.
+#'
+#' * **col_type**: Although you may add any text you desire to the `col_type`
+#' attribute, it is intended to be used to provide additional information above
+#' and beyond the `Data type` (i.e., column class) about the values in the
+#' column. For example, you may have a column of 0's and 1's, which will have
+#' a _numeric_ data type. However, you may want to inform data users that this
+#' is really a dummy variable where the 0's and 1's represent discrete
+#' categories (No and Yes). Another way to think about it is that the
+#' `Data type` attribute is how _R_ understands the column and the `Column type`
+#' attribute is how _humans_ should understand the column.
+#'
+#'    - Perhaps even more importantly, setting the `col_type` attribute helps R
+#'  determine which descriptive statistics to calculate for the bottom half of
+#'  the column attributes table. Inside of the `codebook()` function, the
+#'  `cb_add_summary_stats()` function will attempt to figure out whether the
+#'  column is **numeric**, **categorical - many categories (e.g. participant id)**,
+#'  **categorical - few categories (e.g. sex)**, or **time - including dates**.
+#'  Again, this matters because the table of summary stats shown in the codebook
+#'  document depends on the value `cb_add_summary_stats()` chooses. However, the
+#'  user can directly tell `cb_add_summary_stats()` which summary stats to
+#'  calculate by providing by adding a `col_type` attribute to a column with
+#'  one of the following values: `Numeric`, `Categorical`, or `Time`.
+#'
+#' * **val_labels**: Although you may add any text you desire to the `val_labels`
+#' attribute, it is intended to inform your data users about how to correctly
+#' interpret numerically coded categorical variables. For example, you may have
+#' a column of 0's and 1's that represent discrete categories (i.e., "No" and
+#' "Yes") instead of numerical quantities. In some many other software packages
+#' (e.g., SAS, Stata, and SPSS), you can layer "No" and "Yes" labels on top of
+#' the 0's and 1's to improve the readability of your analysis output. These
+#' are commonly referred to as _value labels_. The R programming language does
+#' not really have value labels in the same way that other popular statistical
+#' software applications do. R users can (and typically should) coerce
+#' numerically coded categorical variables into
+#' [factors](https://www.r4epi.com/numerical-descriptions-of-categorical-variables.html#factor-vectors);
+#' however, coercing to a factor is not the same as adding value labels to a
+#' numeric vector because the underlying numeric values can change in the
+#' process of creating the factor. For this, and other reasons, many R
+#' programmers choose to create a _new_ factor version of a numerically encoded
+#' variable as opposed to overwriting/transforming the numerically encoded
+#' variable. In those cases, you may want to inform your data users about how
+#' to correctly interpret numerically coded categorical variables. Adding value
+#' labels to your codebook is one way of doing so.
+#'
 #' @param df Data frame of interest
 #' @param .x Column of interest in df
 #'
-#' @return A tibble
+#' @return A tibble of column attributes
 #' @importFrom dplyr %>%
 cb_get_col_attributes <- function(df, .x) {
 
   # ===========================================================================
   # Prevents R CMD check: "no visible binding for global variable ‘.’"
   # ===========================================================================
-  value = NULL
+  value = Attribute = NULL
 
   # ===========================================================================
   # Variable management
