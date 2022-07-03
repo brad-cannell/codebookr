4a230bf0e7f9cec5ceef41ff99013cad8694cb22
Brad Cannell <brad.cannell@gmail.com>
2022-07-03 07:31:46 -0700

    Add date_time column to the study data
    Discussed in #9

--- R/data.R
+++ R/data.R
@@ -3,12 +3,13 @@
 #' This is the code to create the study data - a simulated dataset that can be
 #' used to demonstrate how to use the codebook package.
 #'
-#' @format A data frame with 20 rows and 8 variables:
+#' @format A data frame with 20 rows and 9 variables:
 #' \describe{
 #'   \item{id}{Participant's study identification number}
 #'   \item{sex}{Biological sex of the participant assigned at birth, female/male}
 #'   \item{date}{Participant's date of enrollment}
 #'   \item{time}{Participant's time of enrollment}
+#'   \item{date_time}{Participant's date and time of enrollment}
 #'   \item{days}{Total number of days the participant was enrolled in the study}
 #'   \item{height}{Participant's height in inches at date of enrollment}
 #'   \item{likert}{An example Likert scale item, 1-5}
